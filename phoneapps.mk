be5ec5e268d0896d7144a7655cbc1af85864aba9
Prince Paul <prince.paul.k@gmail.com>
2017-02-22 19:35:34 +0530

    upload scripts

---
+++ phoneapps.mk
@@ -0,0 +1,40 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothExt \
+	BluetoothMidiService \
+	CertInstaller \
+	com.qualcomm.location \
+	com.qualcomm.services.location \
+	KeyChain \
+	LiveWallpapers \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	qcrilmsgtunnel \
+	Stk \
+	telresources \
+	TimeService \
+	UserDictionaryProvider \
+	WAPPushManager 
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FMRadio \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	StatementService \
+	TeleService 
