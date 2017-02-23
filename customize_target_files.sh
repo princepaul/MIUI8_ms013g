b7966c0c523f719b799a7d2e037499237d6afd44
Prince Paul <prince.paul.k@gmail.com>
2017-02-23 13:11:21 +0530

    Add device features / customize target files

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -12,6 +12,4 @@ rm -rf out/target_files/SYSTEM/vendor/preinstall
 #Added device features
 cp -f other/ms013g.xml out/target_files/SYSTEM/etc/device_features
 
-#Added multi cust variants for miui
-rm -rf out/target_files/DATA/miui/cust
-cp -rf other/cust out/target_files/DATA/miui
+
